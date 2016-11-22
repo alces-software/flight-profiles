db5e5d36a44093813bf564f723ff68a67de0b765
James Muscat <james.muscat@alces-software.com>
2016-11-22 15:17:36 +0000

    Installer wrapper for STAR-CCM+.

---
+++ features/starccm+/configure.d/run-installer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Wrapper for StarCCM+ installer.
+
+if [[ -f /tmp/STAR-CCM+11.02.010_01_linux-x86_64-r8.tar.gz ]]; then
+  tempdir=$(mktemp -d -t customize-starccm-XXXXXX)
+
+  yum install -y -q redhat-lsb.i686
+
+  pushd "$tempdir"
+
+  tar -xf /tmp/STAR-CCM+11.02.010_01_linux-x86_64-r8.tar.gz
+
+  # -DINSTALLFLEX=false prevents installation of a local license server
+  # -DADDSYSTEMPATH=false prevents root's bash profile being munged - it
+  #    wouldn't affect cluster users' $PATHs anyway
+  starccm+_11.02.010/STAR-CCM*.bin -i silent -DINSTALLDIR=/opt/star-ccm -DINSTALLFLEX=false -DADDSYSTEMPATH=false
+
+  popd
+
+  rm -rf "$tempdir"
+else
+  cat <<EOF
+******************************
+  Please download the STAR-CCM+ installer file and copy it to
+
+  /tmp/STAR-CCM+11.02.010_01_linux-x86_64-r8.tar.gz
+
+  Then, restart the customization process by running
+
+  alces customize trigger configure profile-starccm+
+
+  Installation will then continue.
+
+  A configured license server reachable from the cluster network will be
+  required to run STAR-CCM+.
+
+******************************
+EOF
+fi
