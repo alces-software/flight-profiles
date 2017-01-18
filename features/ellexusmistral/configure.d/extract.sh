0f5c05932092623a2590a829c2695573b5eac5a9
Ruan Ellis <ruanellis@maureen.local>
2017-01-18 14:22:59 +0000

    New Feature; Ellexus Mistral v2.9.4

---
+++ features/ellexusmistral/configure.d/extract.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Wrapper for Ellexus Mistral install.
+
+package=/tmp/mistral_2.9.4_x86_64.zip
+license=/tmp/ellexusmistral-license.lic
+installdir=/opt/ellexusmistral
+version="2.9.4"
+
+if [[ -f $package && -f $license ]]; then
+
+  mkdir -p $installdir
+  cd $installdir
+  echo "Unpacking Ellexus Mistral install files..."
+  unzip -q $package
+  mv mistral_2.9.4_x86_64 $version
+  mkdir -p ${installdir}/${version}/license
+  cp $license ${installdir}/${version}/license/ellexusmistral-license.lic
+  if [[ $? -eq 0 ]]; then
+       mkdir -p "$cw_ROOT"/etc/modules/apps/
+       cp "`dirname "$0"`"/../resources/modulefile "$cw_ROOT/etc/modules/apps/ellexusmistral"
+  cat <<EOF
+******************************
+
+Installation complete. A modulefile has been installed for using Ellexus Mistral . To enable it run:
+
+  alces module load apps/ellexusmistral
+
+You can now delete the installer file by running
+
+  rm /tmp/$package
+  
+******************************
+EOF
+  else
+    echo "Installation failed. See above for error output."
+  fi
+
+else
+  cat <<EOF
+******************************
+  Please download the Ellexus Mistral all-in-one zip archive from 
+  the download link provided when you purchased Mistral.
+  
+  If you do not have a download link, please contact Ellexus Support 
+  at info@ellexus.com.
+
+  Then, please copy the License file and the zip archive to: 
+  
+  /tmp/ellexusmistral-license.lic
+  /tmp/mistral_2.9.4_x86_64.zip 
+  
+  respectively and run the following command:
+
+  alces customize trigger configure feature-ellexusmistral
+
+  Installation will then continue.
+
+******************************
+EOF
+fi
