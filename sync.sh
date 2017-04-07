9ec852412d7cb069a39bb74da73d0872e943f4bb
Mark J. Titorenko <mark.titorenko@alces-software.com>
2017-04-07 12:53:30 +0100

    Allow regions for sync to be overridden using `REGIONS` env var (helpful for release candidates).

--- sync.sh
+++ sync.sh
@@ -62,7 +62,7 @@ else
     prefix_name="($1)"
 fi
 
-regions="eu-west-1 eu-west-2 eu-central-1 ap-northeast-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1"
+regions="${REGIONS:-eu-west-1 eu-west-2 eu-central-1 ap-northeast-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1}"
 
 cd $(dirname "$0")
 for r in $regions; do
